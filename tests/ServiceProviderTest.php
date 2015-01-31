75ad871d240d27ead8e231f5ad51d807dd2d8541
Vincent Klaiber <vincentklaiber@gmail.com>
2015-01-31 20:55:06 +0100

    Add @GrahamCampbell service provider tests.

---
+++ tests/ServiceProviderTest.php
@@ -0,0 +1,9 @@
+<?php namespace Vinkla\Tests\Instagram;
+
+use GrahamCampbell\TestBench\Traits\ServiceProviderTestCaseTrait;
+
+class ServiceProviderTest extends AbstractTestCase {
+
+	use ServiceProviderTestCaseTrait;
+
+}
