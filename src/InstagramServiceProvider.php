82305a71c4e37757d71a94600dba484a92b1e8b2
Vincent Klaiber <vincentklaiber@gmail.com>
2015-10-04 14:56:37 +0200

    Improved Lumen support

--- src/InstagramServiceProvider.php
+++ src/InstagramServiceProvider.php
@@ -29,20 +29,24 @@ class InstagramServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        $this->setupConfig();
+        $this->setupConfig($this->app);
     }
 
     /**
      * Setup the config.
      *
+     * @param \Illuminate\Contracts\Foundation\Application $app
+     *
      * @return void
      */
-    protected function setupConfig()
+    protected function setupConfig(Application $app)
     {
         $source = realpath(__DIR__.'/../config/instagram.php');
 
         if (class_exists('Illuminate\Foundation\Application', false)) {
             $this->publishes([$source => config_path('instagram.php')]);
+        } elseif (class_exists('Laravel\Lumen\Application', false)) {
+            $app->configure('instagram');
         }
 
         $this->mergeConfigFrom($source, 'instagram');
